aaf51c6c1da9b80e28718044f080f01d2d72544b
alazythe <eexepts@gmail.com>
2025-04-01 07:27:56 +0200

    minor changes

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -3,11 +3,18 @@ local opts = { noremap = true, silent = true }
 
 map("n", "<Space>", "<Nop>", opts)
 
--- Example: Telescope
+-- Tabs
+map("n", "<localleader>tn", "<cmd>tabnew<cr>", { noremap = true, silent = true, desc = "New Tab" })
+map("n", "<localleader>tc", "<cmd>tabclose<cr>", { noremap = true, silent = true, desc = "Close Tab" })
+map("n", "<localleader>to", "<cmd>tabonly<cr>", { noremap = true, silent = true, desc = "Close Other Tabs" })
+map("n", "<localleader>o", "<cmd>tabnext<cr>", { noremap = true, silent = true, desc = "Next Tab" })
+map("n", "<localleader>p", "<cmd>tabprevious<cr>", { noremap = true, silent = true, desc = "Prev Tab" })
+
+-- Telescope
 map("n", "<localleader>ff", "<cmd>Telescope find_files<cr>", opts)
 map("n", "<localleader>fg", "<cmd>Telescope live_grep<cr>", opts)
 
--- Example: NvimTree
+-- NvimTree
 map("n", "<localleader>e", "<cmd>NvimTreeToggle<cr>", opts)
 
 -- Example: Lazy plugin manager
--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -30,12 +30,21 @@ require("lazy").setup({
 		"nvim-treesitter/nvim-treesitter",
 		"nvim-tree/nvim-tree.lua",
 		"nvim-tree/nvim-web-devicons",
-		{
-			'nvim-telescope/telescope.nvim',
-			dependencies = { 'nvim-lua/plenary.nvim' },
-		},
 		"m4xshen/autoclose.nvim",
  		"kylechui/nvim-surround",
+		"L3MON4D3/LuaSnip",
+		"saadparwaiz1/cmp_luasnip",
+		"rafamadriz/friendly-snippets",
+		--"SergioRibera/cmp-dotenv",
+		--"lukas-reineke/cmp-under-comparator",
+		{
+			"akinsho/bufferline.nvim",
+			dependencies = { "nvim-tree/nvim-web-devicons" }
+		},
+		{
+			"nvim-telescope/telescope.nvim",
+			dependencies = { "nvim-lua/plenary.nvim" },
+		},
 		{
   			"hrsh7th/nvim-cmp",
   			version = false,
@@ -46,11 +55,6 @@ require("lazy").setup({
 			    "hrsh7th/cmp-path",
 			}
 		},
-		"L3MON4D3/LuaSnip",
-		"saadparwaiz1/cmp_luasnip",
-		"rafamadriz/friendly-snippets",
-		--"SergioRibera/cmp-dotenv",
-		--"lukas-reineke/cmp-under-comparator",
 	},
 	defaults = {
 		lazy = false,
